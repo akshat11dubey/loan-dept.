2964b0a542466aa15182fbceb6c3196f3f1c3d4b
akshat11dubey <159771549+akshat11dubey@users.noreply.github.com>
2024-08-31 17:01:57 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from PIL import Image
+image=Image.open(r'C:\Users\daksh\Downloads\bank.jpeg')
+st.image(image,caption='WELCOME TO VVT BANK')
+st.title('Loan calculator')
+st.header("VVT BANK OF INDIA")
+st.text_input("Enter your name")
+st.text_input("Enter your bank account number ")
+st.text_input("Enter your account type")
+st.text_input("Enter your address")
+st.text_input('Enter your amount')
+y=st.number_input('Enter your salary')
+if y>=50000:
+    st.write('congratulations')
+    st.balloons()
+else:
+     st.write('sorry sir')
+z=st.radio('Are you govt. emp',options=['yes','no'])
+st.checkbox('do you have a credit card')
+st.sidebar.header('schemes from Loan dept')
+st.checkbox('do you have a atm card')
+st.sidebar.markdown(["1~education loan"])
+st.sidebar.markdown(["2~business loan"])
+st.sidebar.markdown(["3~farming loan"])
+st.sidebar.markdown(['4~home loan'])
+st.markdown("""
+<style>
+.big-font {
+    font-size:300px !important;
+}
+</style>
+""", unsafe_allow_html=True)
+menu=['tea':100,'coffee':200,'snacks':300]
+for i in range(menu):
+    st.selectbox('would you like to have somethong',menu(i))
